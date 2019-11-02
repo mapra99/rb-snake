def217c0e310fe45aa0b6c6def6fd3a07941ca27
Miguel Prada <mapra99@gmail.com>
2019-11-01 23:19:48 -0500

    created movement test

--- Gemfile
+++ Gemfile
@@ -4,4 +4,6 @@ source 'https://rubygems.org'
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
+gem 'byebug'
+gem 'minitest'
 gem 'ruby2d', '~> 0.9.2'
--- src/actions/actions.rb
+++ src/actions/actions.rb
@@ -14,25 +14,25 @@ module Actions
 
   private
 
-  def calc_next_position(state)
+  def self.calc_next_position(state)
     curr_position = state.snake.positions.first
     case state.next_direction
-    when UP
+    when Model::Direction::UP
       Model::Coord.new(curr_position.row - 1,
                        curr_position.col)
-    when RIGHT
+    when Model::Direction::RIGHT
       Model::Coord.new(curr_position.row,
                        curr_position.col + 1)
-    when DOWN
+    when Model::Direction::DOWN
       Model::Coord.new(curr_position.row + 1,
                        curr_position.col)
-    when LEFT
+    when Model::Direction::LEFT
       Model::Coord.new(curr_position.row,
                        curr_position.col - 1)
     end
   end
 
-  def position_is_valid?(state, position)
+  def self.position_is_valid?(state, position)
     # verificar que esté en la grilla
     is_invalid = ((position.row >= state.grid.rows) || (position.row < 0)) ||
                  ((position.col >= state.grid.cols) || (position.col < 0))
@@ -43,12 +43,14 @@ module Actions
     !(state.snake.positions.include? position)
   end
 
-  def move_snake_to(state, next_position)
+  def self.move_snake_to(state, next_position)
     new_positions = [next_position] + state.snake.positions[0...-1]
     state.snake.positions = new_positions
+
+    state
   end
 
-  def end_game(state)
+  def self.end_game(state)
     state.game_finished = true
     state
   end
