2d4e5137a9b47541adfd305b3cf326c9f4e698ac
Miguel Prada <mapra99@gmail.com>
2019-11-05 08:52:45 -0500

    added test for valid direction change

--- test/actions_test.rb
+++ test/actions_test.rb
@@ -42,4 +42,17 @@ class ActionsTest < Minitest::Test
     actual_state = Actions::change_direction(@initial_state, Model::Direction::UP)
     assert_equal actual_state, expected_state
   end
+
+  def test_valid_change_direction
+    expected_state = Model::State.new(
+      Model::Snake.new([Model::Coord.new(1, 1),
+                        Model::Coord.new(0, 1)]),
+      Model::Food.new(4, 4),
+      Model::Grid.new(8, 12),
+      Model::Direction::LEFT,
+      false
+    )
+    actual_state = Actions::change_direction(@initial_state, Model::Direction::LEFT)
+    assert_equal actual_state, expected_state
+  end
 end
