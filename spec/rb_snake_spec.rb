c1f45e8a2dfc7078a750d644e835f8cd3eb5c1ee
Miguel Prada <mapra99@gmail.com>
2023-04-16 21:34:35 -0500

    fix spec helper

--- spec/rb_snake_spec.rb
+++ spec/rb_snake_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe RbSnake do
     expect(RbSnake::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  xit "does something useful" do
   end
 end
