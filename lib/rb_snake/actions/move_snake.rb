d12dc2f848c142f84674e43bec1f5a2cd125d8da
Miguel Prada <mapra99@gmail.com>
2023-04-22 17:29:28 -0500

    increase snake speed gradually (#6)

--- lib/rb_snake/actions/move_snake.rb
+++ lib/rb_snake/actions/move_snake.rb
@@ -10,17 +10,24 @@ module RbSnake
           food = state.food
           grid = state.grid
 
+          make_next_move(snake, current_direction, food, grid, state)
+
+          window.render(state)
+        end
+
+        private
+
+        def make_next_move(snake, current_direction, food, grid, state)
           next_position = snake.next_position(current_direction, grid)
           if next_position.eql?(food.location)
             snake.eat(food)
             food.regenerate(snake, grid)
+            state.increase_speed if (snake.body.length % 10).zero?
           elsif snake.body.any? { |body_pos| body_pos.eql?(next_position) }
             state.finish_game!
           else
             snake.move_to(next_position)
           end
-
-          window.render(state)
         end
       end
     end
