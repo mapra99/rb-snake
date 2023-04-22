4130b194e56f9447ea06b3f5ebd69d36560eb849
Miguel Prada <mapra99@gmail.com>
2023-04-22 05:50:14 -0500

    prevent snake from going backwards

--- lib/rb_snake/actions/change_direction.rb
+++ lib/rb_snake/actions/change_direction.rb
@@ -1,11 +1,30 @@
 # frozen_string_literal: true
 
+require "rb_snake/models/direction"
+
 module RbSnake
   module Actions
     class ChangeDirection
       class << self
         def call(state, new_direction:)
+          return unless valid_direction?(state.current_direction, new_direction)
+
           state.update_direction(new_direction)
+        end
+
+        private
+
+        def valid_direction?(current_direction, new_direction)
+          case current_direction
+          when RbSnake::Models::Direction::UP
+            !new_direction.eql?(RbSnake::Models::Direction::DOWN)
+          when RbSnake::Models::Direction::DOWN
+            !new_direction.eql?(RbSnake::Models::Direction::UP)
+          when RbSnake::Models::Direction::LEFT
+            !new_direction.eql?(RbSnake::Models::Direction::RIGHT)
+          when RbSnake::Models::Direction::RIGHT
+            !new_direction.eql?(RbSnake::Models::Direction::LEFT)
+          end
         end
       end
     end
